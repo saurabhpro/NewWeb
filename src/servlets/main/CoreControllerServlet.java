ede374ef5d74eead8f083f46e6ef4d37cb5ec4ee
Saurabh Kumar <saurabhk1511@gmail.com>
2016-05-06 21:12:18 +0530

    servlet extends httpservlet now

--- src/servlets/main/CoreControllerServlet.java
+++ src/servlets/main/CoreControllerServlet.java
@@ -5,6 +5,7 @@ import core.utils.FileFolderWorker;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -13,7 +14,7 @@ import java.io.IOException;
  * Created by kumars on 4/5/2016.
  */
 @WebServlet(name = "CoreControllerServlet", urlPatterns = {"/core"})
-public class CoreControllerServlet extends BackEndLogicController {
+public class CoreControllerServlet extends HttpServlet {
 
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
