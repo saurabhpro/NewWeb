b355af40e940c5462549f6560886f804acd1d6fc
Saurabh Kumar <saurabhk1511@gmail.com>
2016-10-16 13:00:17 +0530

    Updated Holiday list - 14th nov -> 5th sept based on Deepti Achanta's email

--- src/core/model/attendencemodal/HolidaysList.java
+++ src/core/model/attendencemodal/HolidaysList.java
@@ -15,10 +15,10 @@ public enum HolidaysList {
 	NEW_YEAR_DAY(LocalDate.of(2016, Month.JANUARY, 1)), REPUBLIC_DAY(LocalDate.of(2016, Month.JANUARY, 26)), HOLI(
 			LocalDate.of(2016, Month.MARCH, 24)), GOOD_FRIDAY(LocalDate.of(2016, Month.MARCH, 25)), INDEPENDENCE_DAY(
 			LocalDate.of(2016, Month.AUGUST, 15)), RAKSHABANDHAN(
-			LocalDate.of(2016, Month.AUGUST, 18)), VIJAY_DASHMI(
+			LocalDate.of(2016, Month.AUGUST, 18)), TEACHERS_DAY(
+			LocalDate.of(2016, Month.SEPTEMBER, 5)), VIJAY_DASHMI(
 			LocalDate.of(2016, Month.OCTOBER, 11)), DAY_AFTER_DIWALI(
-			LocalDate.of(2016, Month.OCTOBER, 31)), GURU_NANAK_JAYANTI(
-			LocalDate.of(2016, Month.NOVEMBER, 14)), DAY_AFTER_CHRISTMAS(
+			LocalDate.of(2016, Month.OCTOBER, 31)), DAY_AFTER_CHRISTMAS(
 			LocalDate.of(2016, Month.DECEMBER, 26));
 
 	private LocalDate date;
